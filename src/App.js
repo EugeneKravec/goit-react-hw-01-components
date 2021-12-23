2c476d1af6be6a87ccce3d0eedea46ce7f173de9
Eugene Kravec <krava255@gmail.com>
2021-12-23 23:10:16 +0200

    made statistic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import reactDom from "react-dom";
 import Profile from "./components/social-profile/Profile";
 import FriendList from "./components/friend-list/FriendList";
+import Statistics from "./components/statistics/Statistics";
 import user from './refs/user.json';
 import friends from './refs/friends.json';
-// import data from '/путь/к/data.json';
+import data from './refs/data.json';
 
 export default function App() {
     return (
@@ -15,7 +16,13 @@ export default function App() {
                 avatar={user.avatar}
                 stats={user.stats}
             />
-            <FriendList friends={friends} />
+            <FriendList
+                friends={friends}
+            />
+            <Statistics
+                // title="UPLOAD STATS"
+                stats={data}
+            />
         </div>
     )
 }
