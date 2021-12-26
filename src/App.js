5b6020aa07b6df698644ac260fc994d58040e344
Eugene Kravec <krava255@gmail.com>
2021-12-26 22:53:21 +0200

    start css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import reactDom from "react-dom";
+import reactDom from 'react-dom';
+import 'modern-normalize/modern-normalize.css';
 
-import Profile from "./components/social-profile/Profile";
-import FriendList from "./components/friend-list/FriendList";
-import Statistics from "./components/statistics/Statistics";
+import Profile from './components/social-profile/Profile';
+import FriendList from './components/friend-list/FriendList';
+import Statistics from './components/statistics/Statistics';
 import StatisticList from './components/statistics/StatisticList';
 import TransactionHistory from './components/transaction-history/TransactionHistory';
 
@@ -12,24 +13,22 @@ import data from './refs/data.json';
 import transactions from './refs/transactions.json';
 
 export default function App() {
-    return (
-        <div>
-            <Profile
-                username={user.username}
-                tag={user.tag}
-                location={user.location}
-                avatar={user.avatar}
-                stats={user.stats}
-            />
-            <FriendList
-                friends={friends}
-            />
-            <Statistics title="Upload stats">
-                <StatisticList stats={data} />
-            </Statistics>
-            <TransactionHistory items={transactions} />
-        </div>
-    )
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <FriendList friends={friends} />
+      <Statistics title="Upload stats">
+        <StatisticList stats={data} />
+      </Statistics>
+      <TransactionHistory items={transactions} />
+    </div>
+  );
 }
 
-reactDom.render(App, document.getElementById('root'))
+reactDom.render(App, document.getElementById('root'));
--- src/components/social-profile/Profile.js
+++ src/components/social-profile/Profile.js
@@ -1,27 +1,25 @@
 import PropTypes from 'prop-types';
 import ProfileStats from './ProfileStats';
+// import style from './Profile.module.css';
 
 export default function Profile({ avatar, username, tag, location, stats }) {
-    return (
-        <div className="profile">
-            <div className="description">
-                <img
-                    src={avatar}
-                    alt="User avatar"
-                    className="avatar"
-                />
-                <p className="name">{username}</p>
-                <p className="tag">@{tag}</p>
-                <p className="location">{location}</p>
-            </div>
-            <ProfileStats {...stats} />
-        </div>)
+  return (
+    <div className={style.profile}>
+      <div className="description">
+        <img src={avatar} alt="User avatar" className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+      <ProfileStats {...stats} />
+    </div>
+  );
 }
 
 Profile.propTypes = {
-    name: PropTypes.string,
-    tag: PropTypes.string,
-    location: PropTypes.string,
-    avatar: PropTypes.string,
-    stats: PropTypes.objectOf(PropTypes.number),
+  name: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.objectOf(PropTypes.number),
 };
